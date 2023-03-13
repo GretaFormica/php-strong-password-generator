b96e107131f9b2838af0803b7a3b8b598cff33db
GretaFormica <greta.formica@gmail.com>
2023-03-13 17:19:19 +0100

    milestone 2

--- index.php
+++ index.php
@@ -1,18 +1,6 @@
 <?php
 
-    function GeneraPassword($num) {
-
-        $_all_characters = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789!@#$%&?";
-
-        $_password = "";
-
-        for ($i = 0; $i < $num; $i++) {
-            $_casual = rand(0, strlen($_all_characters)-1);
-            $_password .= $_all_characters[$_casual];
-        }
-
-        return $_password;
-    };
+    include 'function.php';
 
 
 ?>
