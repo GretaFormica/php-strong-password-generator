62ca5f8cc81a56f24bd4541ee1a84b8ed1d12a99
GretaFormica <greta.formica@gmail.com>
2023-03-13 17:11:55 +0100

    php + generate pass

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
+<?php
+
+    function GeneraPassword($num) {
+
+        $_all_characters = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789!@#$%&?";
+
+        $_password = "";
+
+        for ($i = 0; $i < $num; $i++) {
+            $_casual = rand(0, strlen($_all_characters)-1);
+            $_password .= $_all_characters[$_casual];
+        }
+
+        return $_password;
+    };
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,9 +27,23 @@
 </head>
 <body>
     <form method="GET">
-        <input type="number" id="num">
-
+        <input type="number" id="number" name="number">
+        <button type="submit">invia</button>
     </form>
+
+    <?php 
+
+        if(isset($_GET["number"])){
+
+            $_number = $_GET["number"];
+            $pass = GeneraPassword($_number);
+
+            echo $pass;
+
+        }
+
+    ?>
+
     
 </body>
 </html>
